69030696c8cbda71a64ff38fe64160a564847aa0
44571782_Chen-YenCHEN <danielchen0810@gmail.com>
2026-03-21 12:37:07 +1000

    Fix autograding test 2

--- main.tf
+++ main.tf
@@ -1,23 +1,40 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
+terraform { 
+   required_providers { 
+      aws = { 
+         source = "hashicorp/aws" 
+         version = "~> 6.0" 
+      } 
+   } 
+} 
+ 
+provider "aws" { 
+   region = "us-east-1" 
+   shared_credentials_files = ["./credentials"] 
+   default_tags { 
+      tags = { 
+         Environment = "Dev" 
+         Course = "CSSE6400" 
+         StudentID = "44571782" 
+      } 
+ } 
+}
+
+resource "aws_instance" "hextris-server" {
+  ami           = "ami-0c421724a94bba6d6"
+  instance_type = "t2.micro"
+  key_name      = "vockey"
+
+  user_data = file("./serve-hextris.sh")
+
+  security_groups = [aws_security_group.hextris-server.name]
+
+  tags = {
+    Name = "hextris"
   }
 }
 
-provider "aws" {
-  region                   = "us-east-1"
-  shared_credentials_files = ["./credentials"]
-
-  default_tags {
-    tags = {
-      Environment = "Dev"
-      Course      = "CSSE6400"
-      StudentID   = "44571782"
-    }
-  }
+output "hextris-url" { 
+ value = aws_instance.hextris-server.public_ip
 }
 
 resource "aws_security_group" "hextris-server" {
@@ -44,31 +61,4 @@ resource "aws_security_group" "hextris-server" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_instance" "hextris-server" {
-  ami           = "ami-0c421724a94bba6d6"
-  instance_type = "t2.micro"
-  key_name      = "vockey"
-
-  user_data = <<-EOF
-#!/bin/bash
-yum install -y httpd
-systemctl enable httpd
-systemctl start httpd
-
-yum install -y git
-cd /var/www/html
-git clone https://github.com/Hextris/hextris .
-EOF
-
-  security_groups = [aws_security_group.hextris-server.name]
-
-  tags = {
-    Name = "hextris"
-  }
-}
-
-output "hextris-url" {
-  value = aws_instance.hextris-server.public_ip
 }
